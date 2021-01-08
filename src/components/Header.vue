0fcea745f6ad129d7b6e6b152f44b6400f300b2e
you <youranning@163.com>
2021-01-08 16:55:23 +0800

    页面制作

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="header">
+<div class="header_bg"></div>
+<div class="header_content">
+    
+</div>
+
+</div>
+</template>
+<script>
+export default {
+    name:'Index',
+    
+}
+</script>
+<style>
+.header_bg{
+    width:100%;
+    height:50px;
+    background: #88C6C6;
+}
+.header_content{
+    height:118px;
+    padding: 0 55px;
+}
+</style>
--- src/view/Index.vue
+++ src/view/Index.vue
@@ -1,9 +1,17 @@
 <template>
-    
+<div id="index">
+<!-- 公共头部 -->
+<header-wrap></header-wrap>
+
+</div>
 </template>
 <script>
+import Header from '../components/Header'
 export default {
-    
+    name:'Index',
+    components:{
+     'header-wrap':Header
+    }
 }
 </script>
 <style>
