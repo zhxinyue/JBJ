a98f040221f723e3d07ba83d4fb3fa04e5c295f2
zhxinyue <code_zhangxinyue@163.com>
2021-01-10 22:55:35 +0800

    首页制作

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,45 @@
 <div id="header">
 <div class="header_bg"></div>
 <div class="header_content">
+    <div class="logo_wrap">
+    <img src="../assets/img/JBJ.png" alt="logo" class="logo_img">
+<img src="../assets/img/JBJ.png" alt="logo" class="logo_img1">
+
+    </div>
     
+    <!-- 未登录 -->
+<el-button class="login_btn">LOG IN</el-button>
+<!-- 搜索 -->
+    <div class="search_wrap">
+        <input type="text" name="" id="" class="search_ipt">
+        <img src="" alt="" class="search_img">
+    </div>
+    <!-- 导航 -->
+    <div class="nav_wrap">
+    <el-dropdown>
+      <span class="el-dropdown-link dropdown_active">
+        HOME
+      </span>
+     
+    </el-dropdown>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        SHOES<i class="el-icon-caret-bottom el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item icon="el-icon-plus">黄金糕</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        SHOPBY<i class="el-icon-caret-bottom el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item icon="el-icon-plus">黄金糕</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+</div>
+  
 </div>
 
 </div>
@@ -10,7 +48,17 @@
 <script>
 export default {
     name:'Index',
-    
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
 }
 </script>
 <style>
@@ -20,7 +68,124 @@ export default {
     background: #88C6C6;
 }
 .header_content{
-    height:118px;
+    width:100%;
+    height:120px;
     padding: 0 55px;
+    box-sizing: border-box;
+}
+.logo_wrap{
+    float: left;
+    width:200px;
+height:120px;
+}
+.logo_img{
+    float: left;
+    width:90px;
+    height:70px;
+    margin-top:25px;
+    margin-right:20px;
+}
+.logo_img1{
+    float: left;
+    width:90px;
+    height:70px;
+    margin-top:25px;
+}
+.header_content .login_btn{
+    float: right;
+    font-size: 30px;
+    height:34px;
+    line-height: 34px;
+    margin-top:50px;
+    padding:0;
+    color:#000;
+    border:0;
+    background: none;
+    margin-left:80px;
+
+}
+.search_wrap{
+    float: right;
+    width:390px;
+    height:34px;
+    margin-top:50px;
+    position: relative;
+}
+.search_ipt{
+    width:390px;
+    height:34px;
+    position: absolute;
+    left:0;
+    top:0;
+    background: #F3F3F3;
+    border-radius: 100px;
+    border:0;
+    
+}
+.search_img{
+    width:24px;
+    height:24px;
+    position: absolute;
+    right:8px;
+    top:5px;
+}
+.nav_wrap{
+    float: right;
+    margin-top:43px;
+    height:40px;
+    background: none;
+}
+.el-dropdown{
+color:#000;
+font-size: 30px;
+margin-right:80px;
+}
+.dropdown_active{
+  font-weight: bold;
+  border-bottom:5px solid #88C6C6;
+  padding-bottom:3px;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.menu_wrap.el-menu--horizontal{
+    border:0;
+}
+.el-menu--horizontal>.el-menu-item,.el-menu--horizontal>.el-submenu{
+    height:40px;
+    line-height: 34px;
+    font-size: 30px;
+    padding: 0;
+margin-right:70px;
+border: none;
+vertical-align: inherit;
+}
+.el-menu--horizontal .el-submenu__title{
+font-size: 30px;
+padding: 0;
+}
+.el-menu--horizontal>.el-submenu .el-submenu__title{
+    height:34px;
+    line-height: 34px;
+    font-size: 30px;
+    border: none;
+}
+.el-menu--horizontal>.el-menu-item.is-active{
+    font-weight: bold;
+    border-bottom:5px solid #88C6C6;
+    /* padding-bottom:10px; */
+    color:#000;
+    line-height: 34px;
 }
 </style>
--- src/view/Index.vue
+++ src/view/Index.vue
@@ -2,7 +2,39 @@
 <div id="index">
 <!-- 公共头部 -->
 <header-wrap></header-wrap>
+<!-- 首页内容 -->
+<div class="index_content">
+<!-- 轮播图 -->
+<el-carousel height="725px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <img src="../assets/img/banner1.png" alt="" class="img_class">
+      </el-carousel-item>
+    </el-carousel>
+    <!-- 推荐产品 -->
+    <div class="recommend_wrap">
+<div class="left_product">
+<img src="../assets/img/img1.png" alt="" class="img_class">
+</div>
+<div class="right_product">
+<img src="../assets/img/img2.png" alt="" class="img_pro_class">
+<img src="../assets/img/img2.png" alt="" class="img_pro_class">
+<img src="../assets/img/img2.png" alt="" class="img_pro_class img_pro_class2">
+<img src="../assets/img/img2.png" alt="" class="img_pro_class img_pro_class2">
+</div>
 
+    </div>
+    <!-- 产品分类 -->
+    <div class="sort_wrap">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+<img src="../assets/img/img3.png" alt="" class="img_sort">
+    </div>
+</div>
 </div>
 </template>
 <script>
@@ -15,5 +47,48 @@ export default {
 }
 </script>
 <style>
+.index_content{
+    padding: 0 55px;
+}
+ 
 
+  .img_class{
+      width:100%;
+      height:100%;
+  }
+  .recommend_wrap{
+      width:100%;
+height:740px;
+margin: 30px 0;
+  }
+  .left_product{
+      float: left;
+      width:895px;
+      height:740px;
+  }
+  .right_product{
+      float: right;
+      width:890px;
+      height:740px;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+  }
+  .img_pro_class{
+      width:430px;
+      height:360px;
+  }
+  .img_pro_class2{
+      margin-top:20px;
+  }
+  .sort_wrap{
+      width:100%;
+      height:120px;
+      display: flex;
+      justify-content: space-between;
+  }
+  .img_sort{
+      width:120px;
+      height:120px;
+  }
 </style>
